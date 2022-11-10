483544273c5882cee6de7e1ee884b76d18260877
i.kłusek <J6ZkqwVUn>
2022-11-10 11:12:26 +0100

    Production Build

---
+++ build/middleware/requireUser.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const requireUser = (req, res, next) => {
+    const user = res.locals.user;
+    console.log(JSON.parse(JSON.stringify(res.locals)));
+    console.log(user);
+    if (!user) {
+        return res.sendStatus(403);
+    }
+    return next();
+};
+exports.default = requireUser;
